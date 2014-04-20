8669dd2be71b58bed3a015ac238ed66011986f96
yt5538528@163.com <yt5538528@163.com>
2014-04-20 11:42:29 +0800

    这个不用保留，运行时可以自动处理，不过首先要配置好NDK环境等环境变量

--- proj.android/obj/local/armeabi/objs/cocos2dx_static/support/ccUtils.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/support/ccUtils.o: \
- /cygdrive/f/yt/soft/cocos2d-x-2.2.3/projects/TouchPro/proj.android/../../../cocos2dx/support/ccUtils.cpp \
- /cygdrive/f/yt/soft/cocos2d-x-2.2.3/projects/TouchPro/proj.android/../../../cocos2dx/support/ccUtils.h
-
-/cygdrive/f/yt/soft/cocos2d-x-2.2.3/projects/TouchPro/proj.android/../../../cocos2dx/support/ccUtils.h:
